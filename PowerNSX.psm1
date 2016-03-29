2dd53dbbfcab9be1c0a94917964242616e0dbe24
Nick Bradford <nick@cloud-geek.com>
2016-03-30 10:12:02 +1100

    calling check-powercliassemblies in module load now.

--- PowerNSX.psm1
+++ PowerNSX.psm1
@@ -38,7 +38,6 @@
 #
 # - Get Edges on LS -> needs get-nsxedge to accept LS as input object
 # - Set/New LR interface functions returning xmldoc, not element.  Check edge interface output as well.
-# - Need to check for PowerCLI we now pretty much require it - which is snapin or module...dang it...
 # - Update Edge (LB? ) validation cmdlets with edgeId
 
 #Requires -Version 3.0
@@ -63,6 +62,58 @@ if ( -not ("TrustAllCertsPolicy" -as [type])) {
 "@
 
 }
+
+
+function Check-PowerCliAsemblies {
+
+    #Checks for known assemblies loaded by PowerCLI.
+    #PowerNSX uses a variety of types, and full operation requires 
+    #extensive PowerCLI usage.  
+    #As of v2, we now _require_ PowerCLI assemblies to be available.
+    #This method works for both PowerCLI 5.5 and 6 (snapin vs module), 
+    #shouldnt be as heavy as loading each required type explicitly to check 
+    #and should function in a modified PowerShell env, as well as normal 
+    #PowerCLI.
+    
+    $RequiredAsm = (
+        "VMware.VimAutomation.ViCore.Cmdlets", 
+        "VMware.Vim",
+        "VMware.VimAutomation.Sdk.Util10Ps",
+        "VMware.VimAutomation.Sdk.Util10",
+        "VMware.VimAutomation.Sdk.Interop",
+        "VMware.VimAutomation.Sdk.Impl",
+        "VMware.VimAutomation.Sdk.Types",
+        "VMware.VimAutomation.ViCore.Types",
+        "VMware.VimAutomation.ViCore.Interop",
+        "VMware.VimAutomation.ViCore.Util10",
+        "VMware.VimAutomation.ViCore.Util10Ps",
+        "VMware.VimAutomation.ViCore.Impl",
+        "VMware.VimAutomation.Vds.Commands",
+        "VMware.VimAutomation.Vds.Impl",
+        "VMware.VimAutomation.Vds.Interop",
+        "VMware.VimAutomation.Vds.Types",
+        "VMware.VimAutomation.Storage.Commands",
+        "VMware.VimAutomation.Storage.Impl",
+        "VMware.VimAutomation.Storage.Types",
+        "VMware.VimAutomation.Storage.Interop",
+        "VMware.DeployAutomation",
+        "VMware.ImageBuilder"
+    )
+
+
+    $CurrentAsmName = foreach( $asm in ([AppDomain]::CurrentDomain.GetAssemblies())) { $asm.getName() } 
+    $CurrentAsmDict = $CurrentAsmName | Group-Object -AsHashTable -Property Name
+
+    foreach( $req in $RequiredAsm ) { 
+
+        if ( -not $CurrentAsmDict.Contains($req) ) { 
+            write-warning "PowerNSX requires PowerCLI."
+            throw "Assembly $req not found.  Some required PowerCli types are not available in this PowerShell session.  Please ensure you are running PowerNSX in a PowerCLI session, or have manually loaded the required assemblies."}
+    }
+}
+
+#Check required PowerCLI assemblies are loaded.
+Check-PowerCliAsemblies
 
 
 ########
@@ -2204,52 +2255,6 @@ function Format-XML () {
 Export-ModuleMember -function Format-Xml
 
 
-function Check-PowerCliAsemblies {
-
-    #Checks for known assemblies loaded by PowerCLI.
-    #PowerNSX uses a variety of types, and full operation requires 
-    #extensive PowerCLI usage.  
-    #As of v2, we now _require_ PowerCLI assemblies to be available.
-    #This method works for both PowerCLI 5.5 and 6 (snapin vs module), 
-    #shouldnt be as heavy as loading each required type explicitly to check 
-    #and should function in a modified PowerShell env, as well as normal 
-    #PowerCLI.
-    
-    $RequiredAsm = (
-        "VMware.VimAutomation.ViCore.Cmdlets", 
-        "VMware.Vim",
-        "VMware.VimAutomation.Sdk.Util10Ps",
-        "VMware.VimAutomation.Sdk.Util10",
-        "VMware.VimAutomation.Sdk.Interop",
-        "VMware.VimAutomation.Sdk.Impl",
-        "VMware.VimAutomation.Sdk.Types",
-        "VMware.VimAutomation.ViCore.Types",
-        "VMware.VimAutomation.ViCore.Interop",
-        "VMware.VimAutomation.ViCore.Util10",
-        "VMware.VimAutomation.ViCore.Util10Ps",
-        "VMware.VimAutomation.ViCore.Impl",
-        "VMware.VimAutomation.Vds.Commands",
-        "VMware.VimAutomation.Vds.Impl",
-        "VMware.VimAutomation.Vds.Interop",
-        "VMware.VimAutomation.Vds.Types",
-        "VMware.VimAutomation.Storage.Commands",
-        "VMware.VimAutomation.Storage.Impl",
-        "VMware.VimAutomation.Storage.Types",
-        "VMware.VimAutomation.Storage.Interop",
-        "VMware.DeployAutomation",
-        "VMware.ImageBuilder"
-    )
-
-
-    $CurrentAsmName = foreach( $asm in ([AppDomain]::CurrentDomain.GetAssemblies())) { $asm.getName() } 
-    $CurrentAsmDict = $CurrentAsmName | Group-Object -AsHashTable -Property Name
-
-    foreach( $req in $RequiredAsm ) { 
-
-        if ( -not $CurrentAsmDict.Contains($req) ) { throw "Assembly $req not found.  Some required PowerCli types are not available in this PowerShell session.  Please ensure you are running PowerNSX in a PowerCLI session, or have manually loaded the required assemblies."}
-    }
-}
-
 
 ##########
 ##########
@@ -2330,9 +2335,6 @@ function Connect-NsxServer {
             [ValidateSet("Continue","Ignore")]
             [string]$ViWarningAction="Continue"   
     )
-
-    #Check required PowerCLI assemblies are loaded.
-    Check-PowerCliAsemblies
 
     if ($PSCmdlet.ParameterSetName -eq "userpass") {      
         $Credential = new-object System.Management.Automation.PSCredential($Username, $(ConvertTo-SecureString $Password -AsPlainText -Force))
