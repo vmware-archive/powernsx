a0cbe9226fb98d726c5eb6116d3b236b91547fed
Nick Bradford <nick@cloud-geek.com>
2016-05-22 18:45:46 -0700

    update sectag tests

--- tests/lbtests.ps1
+++ tests/lbtests.ps1
@@ -1,7 +1,7 @@
 ##Spins up a test ESG and exercises all LB cmdlet functionality
 
 $cl = get-cluster mgmt01
-$ds = get-datastore Data
+$ds = get-datastore MgmtData
 
 #Create one
 $name = "lbtest"
--- tests/sectagtests.ps1
+++ tests/sectagtests.ps1
@@ -40,11 +40,11 @@ write-host "`nSecurity Tags before"
 $vm1 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Added"
-Get-NsxSecurityTag $testSTName1 | New-NsxSecurityTagAssignment -vm ($vm1)
+Get-NsxSecurityTag $testSTName1 | New-NsxSecurityTagAssignment -ApplyToVm -VirtualMachine $vm1
 $vm1 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Removed"
-$vm1 | Remove-NsxSecurityTagAssignment -tag (Get-NsxSecurityTag $testSTName1) -confirm:$false
+$vm1 | Get-NsxSecurityTagAssignment | Remove-NsxSecurityTagAssignment -confirm:$false
 $vm1 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`n--- End Test 1 ---"
@@ -58,11 +58,11 @@ write-host "`nSecurity Tags before"
 $vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Added"
-$vm2 | New-NsxSecurityTagAssignment -tag (Get-NsxSecurityTag $testSTName2) 
+$vm2 | New-NsxSecurityTagAssignment -ApplyTag -SecurityTag $TestST2 
 $vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Removed"
-Get-NsxSecurityTag $testSTName2 | Remove-NsxSecurityTagAssignment -vm ($vm2) -confirm:$false
+$TestSt2 | Get-NsxSecurityTagAssignment | Remove-NsxSecurityTagAssignment -confirm:$false
 $vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`n--- End Test 2 ---"
@@ -76,11 +76,11 @@ write-host "`nSecurity Tags before"
 $vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Added"
-$vm2 | New-NsxSecurityTagAssignment -tag ( Get-NsxSecurityTag | ? { $_.name -like "*testst*" } )
+$vm2 | New-NsxSecurityTagAssignment -ApplyTag -SecurityTag ( Get-NsxSecurityTag | ? { $_.name -like "*testst*" } )
 $vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Removed"
-( Get-NsxSecurityTag | ? { $_.name -like "*testst*" } ) | Remove-NsxSecurityTagAssignment -vm ($vm2) -confirm:$false
+( Get-NsxSecurityTag | ? { $_.name -like "*testst*" } ) | Get-NsxSecurityTagAssignment | Remove-NsxSecurityTagAssignment -confirm:$false
 $vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`n--- End Test 3 ---"
@@ -94,11 +94,11 @@ write-host "`nSecurity Tags before"
 $vm1 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Added"
-( Get-NsxSecurityTag | ? { $_.name -like "*testst*" } ) | New-NsxSecurityTagAssignment -vm ($vm1)
+( Get-NsxSecurityTag | ? { $_.name -like "*testst*" } ) | New-NsxSecurityTagAssignment -ApplyToVm -VirtualMachine $vm1
 $vm1 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Removed"
-$vm1 | Remove-NsxSecurityTagAssignment -tag ( Get-NsxSecurityTag | ? { $_.name -like "*testst*" } )  -confirm:$false
+$vm1 | Get-NsxSecurityTagAssigment | Remove-NsxSecurityTagAssignment -confirm:$false
 $vm1 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`n--- End Test 4 ---"
@@ -109,15 +109,15 @@ write-host -foregroundcolor Green "`n`n`n--- Start Test 5 ---"
 write-host -foregroundcolor Green "`nAdd and remove a single security tag to multiple virtual machines"
 
 write-host "`nSecurity Tags before"
-$vm2,$testVMName1 | Get-NsxSecurityTagAssignment | select name,securitytagname
+$vm1,$vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Added"
-$vm2,$testVMName1 | New-NsxSecurityTagAssignment -tag ( Get-NsxSecurityTag $testSTName2 )
-$vm2,$testVMName1 | Get-NsxSecurityTagAssignment | select name,securitytagname
+$vm1,$vm2 | New-NsxSecurityTagAssignment -ApplTag -SecurityTag ( Get-NsxSecurityTag $testSTName2 )
+$vm1,$vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Removed"
-Get-NsxSecurityTag $testSTName2  | Remove-NsxSecurityTagAssignment -vm ($vm2,$testVMName1) -confirm:$false
-$vm2,$testVMName1 | Get-NsxSecurityTagAssignment | select name,securitytagname
+Get-NsxSecurityTag $testSTName2  | Get-NsxSecurityTagAssignment | Remove-NsxSecurityTagAssignment -confirm:$false
+$vm1,$vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`n--- End Test 5 ---"
 #
@@ -127,48 +127,19 @@ write-host -foregroundcolor Green "`n`n`n--- Start Test 6 ---"
 write-host -foregroundcolor Green "`nAdd and remove a single security tag to multiple virtual machines"
 
 write-host "`nSecurity Tags before"
-$vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
+$vm1,$vm2  | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Added"
-Get-NsxSecurityTag $testSTName2  | New-NsxSecurityTagAssignment -vm ($vm1,$testVMName2)
-$vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
+Get-NsxSecurityTag $testSTName2  | New-NsxSecurityTagAssignment -ApplyToVm -VirtualMachine $vm1,$vm2 
+$vm1,$vm2 | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`nSecurity Tags Removed"
-$vm1,$testVMName2 | Remove-NsxSecurityTagAssignment -tag ( Get-NsxSecurityTag $testSTName2 ) -confirm:$false
-$vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
+$vm1,$vm2  | Get-NsxSecurityTagAssignment | Remove-NsxSecurityTagAssignment -confirm:$false
+$vm1,$vm2  | Get-NsxSecurityTagAssignment | select name,securitytagname
 
 write-host "`n--- End Test 6 ---"
 #
-#
-#
-# write-host -foregroundcolor Green "`n`n`n--- Start Test 7 ---"
-# write-host -foregroundcolor Green "`Testing NSX Security Tag Assignment export"
 
-# write-host "`nSecurity Tags before"
-# $vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
-
-# write-host "`nSecurity Tags Added"
-# Get-NsxSecurityTag $testSTName2  | New-NsxSecurityTagAssignment -vm ($vm1,$testVMName2)
-# $vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
-
-# write-host "`nExporting csv file"
-# $vm1,$testVMName2 | Get-NsxSecurityTagAssignment | export-csv -path $csvFileName
-
-# write-host "`nSecurity Tags Removed"
-# $vm1,$testVMName2 | Remove-NsxSecurityTagAssignment -tag ( Get-NsxSecurityTag $testSTName2 ) -confirm:$false
-# $vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
-
-# write-host "`nImporting security tag assignment from csv file"
-# New-NsxSecurityTagAssignment -csv -path $csvFileName
-
-# write-host "`nSecurity Tags imported"
-# $vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
-
-# write-host "`nSecurity Tags Removed"
-# $vm1,$testVMName2 | Remove-NsxSecurityTagAssignment -tag ( Get-NsxSecurityTag $testSTName2 ) -confirm:$false
-# $vm1,$testVMName2 | Get-NsxSecurityTagAssignment | select name,securitytagname
-
-# write-host "`n--- End Test 7 ---"
 
 #Cleanup
 Get-NsxSecurityTag $testSTName1 | Remove-NsxSecurityTag -confirm:$false
