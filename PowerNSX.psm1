d2dc96e14d64783c6160fa4d9c24199238a4c548
rhancock8 <robbie@thehancocks.me.uk>
2016-09-30 13:45:05 +0100

    Added firewall rule direction support
    Added firewall rule direction support in New-NsxFirewallRule function
    (can be in only , out only or both)

--- PowerNSX.psm1
+++ PowerNSX.psm1
@@ -20275,6 +20275,9 @@ function New-NsxFirewallRule  {
             [ValidateSet("allow","deny","reject")]
             [string]$Action,
         [Parameter (Mandatory=$false)]
+            [ValidateSet("inout","in","out")]
+            [string]$Direction="inout",
+        [Parameter (Mandatory=$false)]
             [ValidateScript({ Validate-FirewallRuleSourceDest $_ })]
             [object[]]$Source,
         [Parameter (Mandatory=$false)]
@@ -20338,6 +20341,7 @@ function New-NsxFirewallRule  {
         #Add-XmlElement -xmlRoot $xmlRule -xmlElementName "sectionId" -xmlElementText $($section.Id)
         Add-XmlElement -xmlRoot $xmlRule -xmlElementName "notes" -xmlElementText $Comment
         Add-XmlElement -xmlRoot $xmlRule -xmlElementName "action" -xmlElementText $action
+        Add-XmlElement -xmlRoot $xmlRule -xmlElementName "direction" -xmlElementText $Direction
         if ( $EnableLogging ) {
             #Enable Logging attribute
             $xmlAttrLog = $xmlDoc.createAttribute("logged")
