070d8a2f551f80188205bda61f1ba71a18117d48
Nick Bradford <nick@cloud-geek.com>
2016-11-05 21:53:54 +1100

    Removed Assembly check

--- PowerNSXCore.psm1
+++ PowerNSXCore.psm1
@@ -33,53 +33,6 @@ $ValidBranches = @("master","v2")
 
 set-strictmode -version Latest
 
-## Custom classes
-
-
-function Check-PowerCliAsemblies {
-
-    #Checks for known assemblies loaded by PowerCLI.
-    #PowerNSX uses a variety of types, and full operation requires 
-    #extensive PowerCLI usage.  
-    #As of v2, we now _require_ PowerCLI assemblies to be available.
-    #This method works for both PowerCLI 5.5 and 6 (snapin vs module), 
-    #shouldnt be as heavy as loading each required type explicitly to check 
-    #and should function in a modified PowerShell env, as well as normal 
-    #PowerCLI.
-    
-    $RequiredAsm = (
-        "VMware.VimAutomation.ViCore.Cmdlets", 
-        "VMware.Vim",
-        "VMware.VimAutomation.Sdk.Util10Ps",
-        "VMware.VimAutomation.Sdk.Util10",
-        "VMware.VimAutomation.Sdk.Interop",
-        "VMware.VimAutomation.Sdk.Impl",
-        "VMware.VimAutomation.Sdk.Types",
-        "VMware.VimAutomation.ViCore.Types",
-        "VMware.VimAutomation.ViCore.Interop",
-        "VMware.VimAutomation.ViCore.Util10",
-        "VMware.VimAutomation.ViCore.Util10Ps",
-        "VMware.VimAutomation.ViCore.Impl",
-        "VMware.VimAutomation.Vds.Commands",
-        "VMware.VimAutomation.Vds.Impl",
-        "VMware.VimAutomation.Vds.Interop",
-        "VMware.VimAutomation.Vds.Types"
-    )
-
-
-    $CurrentAsmName = foreach( $asm in ([AppDomain]::CurrentDomain.GetAssemblies())) { $asm.getName() } 
-    $CurrentAsmDict = $CurrentAsmName | Group-Object -AsHashTable -Property Name
-
-    foreach( $req in $RequiredAsm ) { 
-
-        if ( -not $CurrentAsmDict.Contains($req) ) { 
-            write-warning "PowerNSX requires PowerCLI."
-            throw "Assembly $req not found.  Some required PowerCli types are not available in this PowerShell session.  Please ensure you are running PowerNSX in a PowerCLI session, or have manually loaded the required assemblies."}
-    }
-}
-
-#Check required PowerCLI assemblies are loaded.
-Check-PowerCliAsemblies
 
 ########
 ########
