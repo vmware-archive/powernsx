53527ec93ade4e77579f95e526f80e3fa8edb83c
Nick Bradford <nick@cloud-geek.com>
2017-01-04 19:14:13 +1100

    Ditto last commit

--- PowerNSX.psm1
+++ PowerNSX.psm1
@@ -27,13 +27,8 @@ has its own license that is located in the source code of the respective compone
 #More sophisticated requirement checking done at module load time.
 
 #My installer home and valid PNSX branches (releases) (used in Update-Powernsx.)
-# $PNsxUrlBase = "https://raw.githubusercontent.com/vmware/powernsx"
-# $ValidBranches = @("master","v2")
-
-$PNsxUrlBase = "https://raw.githubusercontent.com/nmbradford/powernsx"
-$ValidBranches = @("PowerNSXCoreInstaller")
-#TODO: Change back to defaults! here, installer as well, url and branch name!
-
+$PNsxUrlBase = "https://raw.githubusercontent.com/vmware/powernsx"
+$ValidBranches = @("master","v2")
 
 $CoreRequiredModules = @("PowerCLI.Vds","PowerCLI.ViCore")
 $DesktopRequiredModules = @("VMware.VimAutomation.Core","VMware.VimAutomation.Vds")
