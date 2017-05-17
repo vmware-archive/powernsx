b227d1d87a78b86f24dcc5391198d22967ff1d4c
Dale Coghlan <dale.coghlan@gmail.com>
2017-05-17 16:25:40 +1000

    Updated tests

--- tests/integration/13.DFWGlobalOptions.Tests.ps1
+++ tests/integration/13.DFWGlobalOptions.Tests.ps1
@@ -42,6 +42,15 @@ Describe "DFW Global Properties" {
         $script:cpsDefault = "100000"
         $script:memorydefault = "100"
         $script:cpudefault = "100"
+        $script:existingFwGlobalOptions = Get-NsxFirewallGlobalConfiguration
+
+        #Set flag used in tests we have to tag out for 6.3.0 and above only...
+        if ( [version]$DefaultNsxConnection.Version -ge [version]"6.2.3" )  {
+            $ver_gt_623 = $true
+        }
+        else {
+            $ver_gt_623 = $false
+        }
 
     }
 
@@ -52,6 +61,10 @@ Describe "DFW Global Properties" {
 
         #TODO: This should take existing and honour it on finish
         Set-NsxFirewallThreshold -Cpu 100 -Memory 100 -ConnectionsPerSecond 100000 | out-null
+
+        # Put the global configuration options back to the way they were before
+        $existingFwGlobalOptions | Set-NsxFirewallGlobalConfiguration
+
         disconnect-nsxserver
     }
 
@@ -100,8 +113,34 @@ Describe "DFW Global Properties" {
 
         }
 
-        it "Can adjust TCP Optimisation"{
-
+        it "Can retrieve DFW Global Configuration Options" {
+            $globalOptions = Get-NsxFirewallGlobalConfiguration
+            $globalOptions | should not be $null
         }
+
+        it "Can Enable TCP Strict Option"{
+            $globalOptions = Get-NsxFirewallGlobalConfiguration
+            $set = $globalOptions | Set-NsxFirewallGlobalConfiguration -EnableTcpStrict | out-null
+            $set.tcpStrictOption | Should be "true"
+        }
+
+        it "Can Disable TCP Strict Option"{
+            $globalOptions = Get-NsxFirewallGlobalConfiguration
+            $set = $globalOptions | Set-NsxFirewallGlobalConfiguration -EnableTcpStrict:$False | out-null
+            $set.tcpStrictOption | Should be "false"
+        }
+
+        it "Can Disable Auto Drafts"  -skip:(-not $ver_gt_623 ) {
+            $globalOptions = Get-NsxFirewallGlobalConfiguration
+            $set = $globalOptions | Set-NsxFirewallGlobalConfiguration -DisableAutoDraft | out-null
+            $set.tcpStrictOption | Should be "true"
+        }
+
+        it "Can Disable TCP Strict Option"  -skip:(-not $ver_gt_623 ) {
+            $globalOptions = Get-NsxFirewallGlobalConfiguration
+            $set = $globalOptions | Set-NsxFirewallGlobalConfiguration -DisableAutoDraft:$False | out-null
+            $set.tcpStrictOption | Should be "false"
+        }
+
     }
 }
