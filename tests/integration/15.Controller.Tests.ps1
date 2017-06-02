0903cd3c655c0e1116644163ff7c10f15893c031
Nick Bradford <nick@cloud-geek.com>
2017-06-02 16:47:04 +1000

    Adding TZ and Controller Tests

---
+++ tests/integration/15.Controller.Tests.ps1
@@ -0,0 +1,51 @@
+#Do not remove this - we need to ensure connection setup and module deps preload have occured.
+If ( -not $PNSXTestVC ) {
+    Throw "Tests must be invoked via Start-Test function from the Test module.  Import the Test module and run Start-Test"
+}
+
+Describe "Controller" {
+
+    BeforeAll {
+
+        #BeforeAll block runs _once_ at invocation regardless of number of tests/contexts/describes.
+        #We load the mod and establish connection to NSX Manager here.
+        import-module $pnsxmodule
+        $script:DefaultNsxConnection = Connect-NsxServer -vCenterServer $PNSXTestVC -Credential $PNSXTestDefViCred -ViWarningAction "Ignore"
+        $script:ctrl = Get-NsxController
+        if ( @($ctrl).count -ne 1) {
+            $script:SkipCtrlTest = $True
+            write-warning "NSX Controller tests disabled.  Ensure only a single controller is deployed to enable controller tests."
+        }
+        else {
+            $ctrlview = Get-View "$($Ctrl.virtualMachineInfo.objectTypeName)-$($Ctrl.virtualMachineInfo.objectId)" -Property Network,Datastore,resourcepool
+            $script:rp = Get-ResourcePool -Id ("$($ctrlview.ResourcePool.type)-$($ctrlview.ResourcePool.Value)")
+            $script:pg = Get-VdPortGroup -Id ("$($ctrlview.Network.type)-$($ctrlview.Network.Value)")
+            $script:ds = Get-Datastore -Id ("$($ctrlview.Datastore.type)-$($ctrlview.Datastore.Value)")
+
+            #Yup - this is dodgy... going to create Find-NsxControllerIpPool using ip matchs to search existing pools one of these days, as you cant reverse engineer this from the controller API response. :(
+            $script:pool = Get-NSxIpPool | ? { $_.name -match "controller" }
+            write-warning "Using existing controller ds, portgroup and resourcepool config for additional controller deployment"
+            $script:SkipCtrlTest = $False
+        }
+    }
+
+    It "Can deploy a controller - Sync" -Skip:$SkipCtrlTest {
+        $newctrl = New-NsxController -IpPool $pool -ControllerName pester_test_ctrl_1 -ResourcePool $rp -Datastore $ds -PortGroup $pg -wait
+        $currentCtrl = Get-NsxController -ObjectId $newctrl.objectid
+        $currentCtrl | should notbe $null
+        $currentCtrl.status | should be "RUNNING"
+    }
+
+    It "Can remove a controller - Sync" -Skip:$SkipCtrlTest {
+        $ctrlToRemove = Get-NSxController | ? { $_.objectId -ne $ctrl.objectId }
+        {$CtrlToRemove | Remove-NsxController -Wait } | should not throw
+        Get-NsxController -ObjectId $ctrlToRemove.objectid  | should be $null
+    }
+    AfterAll {
+
+        #AfterAll block runs _once_ at completion of invocation regardless of number of tests/contexts/describes.
+        #We kill the connection to NSX Manager here.
+        disconnect-nsxserver
+    }
+}
+
