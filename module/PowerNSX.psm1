791953564d2585984ad95d8cdaca9b0755e5882f
Stefan Pahrmann <stefan@pahrmann.dk>
2017-11-29 08:49:16 +0100

    Fixed New-NsxLoadBalancerPool

--- module/PowerNSX.psm1
+++ module/PowerNSX.psm1
@@ -30253,8 +30253,9 @@ function New-NsxLoadBalancerPool {
             write-warning "Load Balancer feature is not enabled on edge $($edgeId). Use Set-NsxLoadBalancer -Enabled to enable."
         }
 
-        [System.XML.XMLElement]$xmlPool = $_LoadBalancer.OwnerDocument.CreateElement("pool")
-        $_LoadBalancer.appendChild($xmlPool) | out-null
+        [System.XML.XmlDocument]$xmldoc = New-Object System.XML.XmlDocument
+        [System.XML.XMLElement]$xmlPool = $xmldoc.CreateElement("pool")
+        $xmldoc.appendChild($xmlPool) | out-null
 
         Add-XmlElement -xmlRoot $xmlPool -xmlElementName "name" -xmlElementText $Name
         Add-XmlElement -xmlRoot $xmlPool -xmlElementName "description" -xmlElementText $Description
@@ -30272,11 +30273,11 @@ function New-NsxLoadBalancerPool {
             }
         }
 
-        $URI = "/api/4.0/edges/$EdgeId/loadbalancer/config"
+        $URI = "/api/4.0/edges/$EdgeId/loadbalancer/config/pools"
         $body = $_LoadBalancer.OuterXml
 
         Write-Progress -activity "Update Edge Services Gateway $($EdgeId)" -status "Load Balancer Config"
-        $null = invoke-nsxwebrequest -method "put" -uri $URI -body $body -connection $connection
+        $null = invoke-nsxwebrequest -method "post" -uri $URI -body $body -connection $connection
         write-progress -activity "Update Edge Services Gateway $($EdgeId)" -completed
 
         $UpdatedEdge = Get-NsxEdge -objectId $($EdgeId) -connection $connection
